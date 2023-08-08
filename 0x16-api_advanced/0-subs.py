cae13bf9f2259b8d1eb91b7f6f4e9d4293dc5162
puppykiwi <johnraymwendwa04@gmail.com>
2023-08-08 10:07:42 +0300

    try/except

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,23 +2,25 @@
 """ QUery the Reddit API and returns the number of subscribers for a given subreddit."""
 
 from os import getenv
-import praw
+import praw, prawcore
 import requests
 from dotenv import load_dotenv
 from sys import argv
 
 load_dotenv()
-reddit = praw.Reddit(
-    client_id=getenv('CLIENT'),
-    client_secret=getenv('SECRET'),
-    user_agent=getenv('AGENT')
-)
-
 def number_of_subscribers(subreddit):
-    subreddit = reddit.subreddit(subreddit)
-    if subreddit is None:
+    
+    reddit = praw.Reddit(
+        client_id=getenv('CLIENT'),
+        client_secret=getenv('SECRET'),
+        user_agent=getenv('AGENT')
+    )
+
+    try:
+        subreddit = reddit.subreddit(subreddit)
+        return subreddit.subscribers
+    except (prawcore.exceptions.NotFound, prawcore.exceptions.Redirect):
         return 0
-    return subreddit.subscribers
 
 if __name__ == "__main__":
     print(number_of_subscribers(argv[1]))
