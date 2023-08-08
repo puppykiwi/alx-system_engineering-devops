1f5347dbe89408a5d4d98cf5e8bb8343acdbe80e
puppykiwi <johnraymwendwa04@gmail.com>
2023-08-08 16:26:25 +0300

    kk100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,50 +3,46 @@
 titles of all hot articles for a given subreddit."""
 
 
-import praw
-import prawcore
-from os import getenv
-from dotenv import load_dotenv
-load_dotenv()
+import requests
 
 
 def count_words(subreddit_name, word_list, word_counts=None, after=None):
     if word_counts is None:
         word_counts = {}
+    
+    base_url = f"https://www.reddit.com/r/{subreddit_name}/hot.json"
+    params = {"limit": 100, "after": after, "allow_redirects": False}
+    headers = {'user-agent': 'custom'}
 
-    reddit = praw.Reddit(
-    
-        client_id=getenv("CLIENT"),
-        client_secret=getenv('SECRET'),
-        user_agent=getenv('AGENT'),
-        user_name=getenv('USER'),
-        password=getenv('PASS')
-    )
-    
-    try:
-        subreddit = reddit.subreddit(subreddit_name)
-        hot_posts = subreddit.hot(limit=None, params={'after': after})
-        
-        for post in hot_posts:
-            title_words = post.title.lower().split()
+    response = requests.get(base_url, params=params, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        children = data.get("data", {}).get("children", [])
+
+        for child in children:
+            title = child.get("data", {}).get("title", "").lower()
+            print(title) #debug
             for word in word_list:
-                if word.lower() in title_words:
+                if word.lower() in title:
                     if word in word_counts:
                         word_counts[word] += 1
                     else:
                         word_counts[word] = 1
 
-        if hot_posts.params.get('after'):
-            return count_words(subreddit_name, word_list, word_counts, after=hot_posts.params['after'])
+        after = data.get("data", {}).get("after")
+        if after:
+            return count_words(subreddit_name, word_list, after, word_counts)
         else:
             sorted_counts = sorted(word_counts.items(), key=lambda item: (-item[1], item[0]))
             for word, count in sorted_counts:
                 print(f"{word}: {count}")
-    except (prawcore.exceptions.NotFound, prawcore.exceptions.Redirect):
+    elif response.status_code == 404 or response.status_code == 302:
         return None
 
+
 if __name__ == "__main__":
-    subreddit_name = "kenya"  # Replace with your desired subreddit
-    keywords = ["python", "javascript", "java"]  # Replace with your desired keywords
-    #count_words(subreddit_name, keywords)
-    
+    subreddit_name = "kenya"
+    keywords = ["python"]
+    count_words(subreddit_name, keywords)
+
