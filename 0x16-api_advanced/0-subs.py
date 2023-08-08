b8a9a626baf1d19ce754d823c885b9304eb53919
puppykiwi <johnraymwendwa04@gmail.com>
2023-08-08 15:56:45 +0300

    task 0 fix million

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,22 +13,33 @@ load_dotenv()
 
 
 
-def number_of_subscribers(subreddit_name):
-    user_agent = getenv("AGENT")
-    
-    url = f"https://www.reddit.com/r/{subreddit_name}/about.json"
-    headers = {"User-Agent": user_agent}
-    
-    response = requests.get(url, headers=headers)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
-        return 0
+def number_of_subscribers(subreddit):
+    """
+    Args:
+        subreddit (str): subreddit
+
+    Returns:
+        int: number of subscribers
+    """
+    base_url = 'https://www.reddit.com/r/'
+
+    url = '{}{}/about.json'.format(base_url, subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    results = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if results.status_code == 200:
+        return results.json()['data']['subscribers']
+    return 0
 
 
 if __name__ == "__main__":
     subreddit_name = argv[1]  # Replace with your desired subreddit
-    subscribers = number_of_subscribers(subreddit_name="intp")
+    subscribers = number_of_subscribers(subreddit="intp")
     print(f"Subreddit '{subreddit_name}' has {subscribers} subscribers.")
