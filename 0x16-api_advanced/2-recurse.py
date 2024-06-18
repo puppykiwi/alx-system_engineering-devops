7989c42ac0fc0da255277cf24681c9bef76ab4ad
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-06-18 10:43:11 +0300

    progress

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,10 +3,10 @@
 import requests
 import sys
 
-def recurse(subreddit, count = 0, after = None):
+def recurse(subreddit, count = 0, after = None, limit = 20):
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {"limit": "20", "after": after, "count": count}
+    params = {"limit": limit, "after": after, "count": count}
     all_posts = []
 
     response = requests.get(url, params)
@@ -20,10 +20,16 @@ def recurse(subreddit, count = 0, after = None):
         for post in posts:
             post_data = post.get("data", {})
             post_title = post_data.get("title")
+            all_posts.append(post_title)
             print (post_title)
+            count += 1
         
     else:
         print("Err code: ", response.status_code)
+
+    if after and count < limit:
+            all_posts.extend(recurse(subreddit, count, after, limit))
+
     return all_posts
 
 if __name__ == '__main__':
