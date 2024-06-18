02bdef0310ad681f53e7f15d4e1609a6febd0e3b
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-06-18 09:48:10 +0300

    url

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,13 +2,16 @@ import sys
 import requests
 
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/subreddit/about"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
     
     response = requests.get(url, subreddit)
 
     if response.status_code == 200:
-        print("call successful")
+        # print("call successful")
+        # print(response.json)
         data = response.json()
+
         return data
     else:
         print("Er code: ", response.status_code)
