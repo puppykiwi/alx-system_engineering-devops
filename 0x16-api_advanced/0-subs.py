82ddf6580deaa66f7617501b0709d91fc71c7879
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-06-18 14:13:38 +0300

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -33,14 +33,3 @@ def number_of_subscribers(subreddit):
     else:
         print("Err code: ", response.status_code)
         return 0
-
-
-if __name__ == '__main__':
-    """
-    # number_of_subscribers = __import__('0-subs').number_of_subscribers
-    """
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
