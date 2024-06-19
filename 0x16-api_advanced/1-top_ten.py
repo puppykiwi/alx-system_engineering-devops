2cb760d30378c0a61e58c540d071c73f48497c83
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-06-19 10:29:42 +0300

    t1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,55 +1,41 @@
 #!/usr/bin/python3
 '''A module containing functions for working with the Reddit API.
 '''
-
 import requests
-import sys
-"""
-This program will find and print the no of subscribers in a specified
-reddit subreddit
-"""
+
+
+BASE_URL = 'https://www.reddit.com'
+'''Reddit's base API URL.
+'''
 
 
 def top_ten(subreddit):
-    """
-    Calls and formats a stribng from a json result
-    """
-    url = "https://www.reddit.com"
-    params = {"limit": "10"}
-    headers = {"User-Agent": "Mozilla/5.0"}
-
-    response = requests.get('{}/r/{}/hot.json'
-                            .format(url, subreddit),
-                            headers=headers, params=params,
-                            allow_redirects=False)
-
-    if response.status_code == 200:
-        # print("call successful")
-        # print(response.json)
-        data = response.json()
-        posts = data.get('data', {}).get('children', [])
-        for post in posts:
-            post_data = post.get('data', {})
-            title = post_data.get('title')
-            score = post_data.get('score')
-            permalink = post_data.get('permalink')
-            url = post_data.get('url')
-            print("Title: {}\nScore: {}\nURL:{}\nPermalink: {}\n"
-                  .format(title, score, url, permalink))
-
-        else:
-            print("Unexpected JSON structure")
-            return 0
-
+    '''Retrieves the title of the top ten posts from a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    sort = 'top'
+    limit = 10
+    res = requests.get(
+        '{}/r/{}/.json?sort={}&limit={}'.format(
+            BASE_URL,
+            subreddit,
+            sort,
+            limit
+        ),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        for post in res.json()['data']['children'][0:10]:
+            print(post['data']['title'])
     else:
-        if response.status_code == 404:
-            print("None")
-        print("Err code: ", response.status_code)
-        return 0
-
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+        print(None)
