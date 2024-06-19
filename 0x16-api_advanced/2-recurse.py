1af8eaf2a8204a64fb43b3fc87160261ddb514b1
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-06-19 10:12:15 +0300

    bien

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python3
+'''A module containing functions for working with the Reddit API.
+'''
 
 import requests
 import sys
+"""
+This program will find and print the no of subscribers in a specified
+reddit subreddit
+"""
 
-def recurse(subreddit, count = 0, after = None, limit = 100, all_posts = None):
 
+def recurse(subreddit, count=0, after=None, limit=100, all_posts=None):
+    """
+    Calls and formats a stribng from a json result
+    """
     if all_posts is None:
         all_posts = []
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = "https://www.reddit.com"
     params = {"limit": limit, "after": after, "count": count}
+    headers = {"User-Agent": "Mozilla/5.0"}
 
-    response = requests.get(url, params)
+    response = requests.get('{}/r/{}/hot.json'
+                            .format(url, subreddit),
+                            headers=headers, params=params,
+                            allow_redirects=False)
 
     if response.status_code == 200:
         # print("success")
         data = response.json()
-        posts = data.get('data', {}).get('children', []) 
+        posts = data.get('data', {}).get('children', [])
         after = data.get('data', {}).get('after')
 
         for post in posts:
             post_data = post.get("data", {})
             post_title = post_data.get("title")
             all_posts.append(post_title)
-            print (post_title)
+            print(post_title)
             count += 1
-        
+
     else:
         print("Err code: ", response.status_code)
 
