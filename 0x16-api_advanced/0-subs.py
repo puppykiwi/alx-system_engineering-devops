bb9a54dea63bcfb634e60ae8bb09f60a4f56f453
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-06-19 09:46:01 +0300

    bak

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+'''A module containing functions for working with the Reddit API.
+'''
+
 import requests
+import sys
+"""
+This program will find and print the no of subscribers in a specified
+reddit subreddit
+"""
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """
+    Calls and formats a stribng from a json result
+    """
+    url = "https://www.reddit.com"
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    response = requests.get('{}/r/{}/about.json'
+                            .format(url, subreddit),
+                            headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        # print("call successful")
+        # print(response.json)
+        data = response.json()
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            print("Unexpected JSON structure")
+            return 0
+    else:
+        print("Err code: ", response.status_code)
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
