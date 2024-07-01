d5e9c5e538dc2c17675902d4cc455b0fc7bf9bc4
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-07-01 15:38:25 +0300

    undo

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,5 @@
 # install puppet-lint
-
-# Install Python
-package { 'python3':
-  ensure => installed,
-}
-
-# Install Pip
-package { 'python3-pip':
-  ensure => installed,
-  require => Package['python3'],
-}
-
-# Install a Python module using Pip
-package { 'requests':
-  ensure   => installed,
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem'
 }
