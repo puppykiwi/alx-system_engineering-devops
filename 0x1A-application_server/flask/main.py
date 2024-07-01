b3ab89e46528240f63e14ca74df692e606578286
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-07-01 14:56:38 +0300

    task 0

--- 0x1A-application_server/flask/main.py
+++ 0x1A-application_server/flask/main.py
@@ -6,7 +6,7 @@ app = Flask('__name__')
 
 def index():
     return render_template('index.html', 
-                           create_name='Hello') 
+                           create_name='Hello tester') 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
