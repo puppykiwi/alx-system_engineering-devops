b7f476e3eb1ddfa53c42e24e6c9efeca48f3bb0e
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-07-03 08:09:46 +0300

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,6 @@
 # repair an nginx webserver that's failing too many requests
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
